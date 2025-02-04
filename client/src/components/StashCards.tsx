12b7c6c573e43a449e6e52d53d8b60e2ff588ab6
BodartA <a.bodart23@gmail.com>
2025-02-04 19:38:41 +0100

    Get request base set to load data on Stash Page

--- client/src/components/StashCards.tsx
+++ client/src/components/StashCards.tsx
@@ -1,20 +1,29 @@
-import { Plus, Vault } from 'lucide-react';
+import { Plus } from 'lucide-react';
 import React from 'react';
 
-const StashCards = () => {
+interface StashCardsProps {
+  index: number,
+  name: string,
+  totalPaints: number,
+}
+
+const StashCards: React.FC<StashCardsProps> = ({ index, name, totalPaints }) => {
   return (
     <div>
       <div className='flex flex-col bg-black outline-2 rounded-lg font-montserrat w-[200px]'>
         <div className=' -translate-y-1 hover:translate-y-0 transition-all outline-2 rounded-lg'>
           <div className=' flex h-[64px] border-b-[1px] rounded-t-lg border-dashed bg-pant-red items-center justify-center'>
-            <h1 className=' font-bold text-xl uppercase'>Vault 1</h1>
+            <h1 className=' font-bold text-xl uppercase'>Vault {index + 1}</h1>
           </div>
           <div className=' bg-cream  rounded-b-lg p-2 border-t-[1px] border-dashed'>
-            <h3 className=' text-center font-bold uppercase pb-2'>Ma Boyz</h3>
-            <p>Vallejo : 1</p>
+            <h3 className=' text-center font-bold uppercase pb-2'>{name}</h3>
+            {/* <p>Vallejo : 1</p>
             <p>GW : 2</p>
-            <p>AK Paints : 32</p>
-            <p className=' font-semibold'>Total : 35</p>
+            <p>AK Paints : 32</p> */}
+            {
+              totalPaints === 0 ? (<p className=' text-center'>No paints yet :(</p>) : (<p className=' font-semibold'>Total : {totalPaints}</p>)
+            }
+
           </div>
         </div>
       </div>
--- client/src/pages/Stash.tsx
+++ client/src/pages/Stash.tsx
@@ -5,6 +5,7 @@ import StashCards, { CreateVaultCard } from '../components/StashCards';
 
 interface Stash {
   name: string,
+  paints: { name: string, brand: string, ref: string }[]
 }
 
 
@@ -14,22 +15,37 @@ const Stash = () => {
 
   useEffect(() => {
 
-    const fetchData = async () => {
+    const fetchStashData = async () => {
       try {
+        const res = await fetch('http://localhost:3000/api/vault')
+        const data = await res.json()
 
+        setStash(data)
       } catch (error) {
-
+        console.log(error)
       }
     }
-    fetchData()
+    fetchStashData()
   }, [])
 
   return (
     <div className='flex flex-col min-h-screen w-screen'>
       <Navbar />
       <h1 className=' flex justify-center p-4 font-extrabold font-montserrat text-5xl text-center'>Welcome to your stash !<br /> Let's see what's in there.</h1>
-      <div className=' flex flex-row p-4 gap-4 justify-center flex-grow'>
-        <Link><StashCards /></Link>
+      <div className=' flex flex-row flex-wrap p-4 gap-4 justify-center'>
+        {
+          stash.map((vault, index) => (
+            <Link key={index} to='/'>
+              <StashCards index={index} name={vault.name} totalPaints={vault.paints.length} />
+            </Link>
+          ))
+        }
+
+
+
+
+
+
         <Link to='/newvault'><CreateVaultCard /></Link>
       </div>
     </div >
